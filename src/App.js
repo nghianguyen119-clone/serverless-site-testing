b398a1753d57d99c7768f30d6f4967cba47cccb7
Trong Nghia <trongnghia.nguyen@outlook.com>
2021-07-14 13:35:27 +0200

    Up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Header = () => {
+	return (
+		<div>
+			<p>Header</p>
+		</div>
+	);
+};
 
-export default App;
+const Homepage = () => {
+	return (
+		<div>
+			<h1>Homepage </h1>
+			<Link to="/about">Go to Aboutpage</Link>
+		</div>
+	);
+};
+
+const Aboutpage = () => {
+	return (
+		<div>
+			<h1>Aboutpage</h1>
+			<Link to="/">Go to Aboutpage</Link>
+		</div>
+	);
+};
+export { Homepage, Aboutpage, Header }; //new
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Homepage, Aboutpage, Header } from "./App"; //new
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Router>
+		<Header />
+		<Switch>
+			<Route exact path="/" component={Homepage} />
+			<Route exact path="/about" component={Aboutpage} />
+		</Switch>
+	</Router>,
+	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
