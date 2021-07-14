3981c34f99e2dc645da3b03595b404bfc2e130d5
Trong Nghia <trongnghia.nguyen@outlook.com>
2021-07-14 14:59:18 +0200

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Homepage, Aboutpage, Header } from "./App"; //new
 
 ReactDOM.render(
